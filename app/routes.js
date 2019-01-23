16580e6e1963a37142f120e2b0241e36aa27d3d9
“Laura <>
2019-01-23 15:22:14 +0000

    “HTBHF-72addrevealtopregnantqandaddqtocheckanswers”

--- app/routes.js
+++ app/routes.js
@@ -18,21 +18,6 @@ router.post('/apply/pregnant1-answer', function (req, res) {
   }
 })
 
-// Are you pregnant Branching 
-router.post('/apply/pregnant-answer', function (req, res) {
-  // Get the answer from session data
-  // The name between the quotes is the same as the 'name' attribute on the input elements
-  // However in JavaScript we can't use hyphens in variable names
-
-  let pregnantyes = req.session.data['pregnant']
-
-  if (pregnantyes === 'false') {
-    res.redirect('/apply/live-with-partner')
-  } else {
-    res.redirect('/apply/due-date')
-  }
-})
-
 // Live with a partner Branching 
 router.post('/apply/live-with-partner-answer', function (req, res) {
   // Get the answer from session data
