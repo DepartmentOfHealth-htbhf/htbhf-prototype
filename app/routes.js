44a7a4539dbcd2f58252b2e39ee01ea07fd281ce
“Laura <>
2019-02-01 14:40:50 +0000

    “HTBHF-72branchingfixandconditionalsectiononcheckanswers”

--- app/routes.js
+++ app/routes.js
@@ -18,19 +18,23 @@ router.post('/apply/pregnant1-answer', function (req, res) {
   }
 })
 
-// Live with a partner Branching 
+// live with partner branching
+
 router.post('/apply/live-with-partner-answer', function (req, res) {
-  // Get the answer from session data
-  // The name between the quotes is the same as the 'name' attribute on the input elements
-  // However in JavaScript we can't use hyphens in variable names
 
-  let partneryes = req.session.data['live-with-partner']
+  // Make a variable and give it the value from 'juggling-balls'
+  var livePartner  = req.session.data['live-with-partner']
 
-  if (partneryes === 'false') {
-    res.redirect('/apply/manual-card-address')
-  } else {
+  // Check whether the variable matches a condition
+  if (livePartner == "Yes"){
+    // Send user to next page
     res.redirect('/apply/does-partner-have-nino')
   }
+  else {
+    // Send user to address page
+    res.redirect('/apply/manual-card-address')
+  }
+
 })
 
 // Partner has a NINO Branching 
@@ -56,6 +60,8 @@ router.get('/apply/check-your-answers', function (req, res) {
   res.render('apply/check-your-answers',{CheckAnswers})
 })
 
+
+
 // JUGGLING
 
 router.post('/juggling-licence/juggling-balls-answer', function (req, res) {
@@ -75,24 +81,6 @@ router.post('/juggling-licence/juggling-balls-answer', function (req, res) {
 
 })
 
-// Live with partner
-
-router.post('/juggling-licence/juggling-balls-answer', function (req, res) {
-
-  // Make a variable and give it the value from 'juggling-balls'
-  var jugglingBalls = req.session.data['juggling-balls']
-
-  // Check whether the variable matches a condition
-  if (jugglingBalls == "3 or more"){
-    // Send user to next page
-    res.redirect('/juggling-licence/juggling-trick')
-  }
-  else {
-    // Send user to ineligible page
-    res.redirect('/juggling-licence/ineligible')
-  }
-
-})
 
 
 module.exports = router
