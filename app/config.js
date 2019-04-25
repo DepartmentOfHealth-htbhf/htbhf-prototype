668e9e638eacfa4b13fa27db4a7bad8f55586654
“Laura <laurafritzsche@Lauras-MacBook-Pro.local>
2019-04-25 11:49:23 +0100

    service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Test prototype',
+  serviceName: 'Help to buy healthy food',
 
   // Default port that prototype runs on
   port: '3000',
