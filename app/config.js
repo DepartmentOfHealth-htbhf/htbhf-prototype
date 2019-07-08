c8571b9871d61a31efb8833b40379f9cd787023e
“Laura <laurafritzsche@Lauras-MBP.localdomain>
2019-07-08 10:06:20 +0100

    Add 'Apply for Healthy Start' to service banner

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Healthy Start',
+  serviceName: 'Apply for Healthy Start',
 
   // Default port that prototype runs on
   port: '3000',
